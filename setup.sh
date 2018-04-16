8b943ad3a4c3690d88d61f96d12496054360579f
Marcel Aldecoa <marcel.aldecoa@microsoft.com>
2018-04-15 23:56:45 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+sudo -s
+mkdir -p /setup-dl-lab-temp
+cd /setup-dl-lab-temp
+wget https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh
+bash Anaconda3-5.1.0-Linux-x86_64.sh -b -p $HOME/anaconda3 -u
+export PATH="$HOME/anaconda3/bin:$PATH"
+source ~/.bashrc
